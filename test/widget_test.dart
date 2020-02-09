7fc31c8fe2c60e5ac148737a2c47b6127ea00d47
Hieu Tran Trung <hieu.trantrung@aux.dkatalis.com>
2020-02-09 23:06:42 +0700

    + add unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,11 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and trigger a frame.await
+    await tester.pumpWidget(MyApp());
   });
 }
